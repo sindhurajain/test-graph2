c2f10efad3fe63af6c89e5692b4f4563525d0084
Sindhura Jain <115209308+sindhurajain@users.noreply.github.com>
2025-04-01 15:00:00 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { BarChart } from "@mui/x-charts/BarChart";
+
+export default function App() {
+  return (
+    <BarChart
+      series={[
+        { data: [35, 44, 24, 34], label: "Series 1" },
+        { data: [51, 6, 49, 30], label: "Series 2" },
+        { data: [15, 25, 30, 50], label: "Series 3" },
+        { data: [50, 50, 15, 25], label: "Series 4" },
+      ]}
+      height={400}
+      xAxis={[{ data: ["Q1", "Q2", "Q3", "Q4"], scaleType: "band" }]}
+      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+    />
+  );
+}
